b27b6e8fa63724b96ac52897e8208a4ad7fccc6d
techiro <techiromen0@gmail.com>
2022-04-03 18:30:19 +0900

    installページの作成

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,49 @@
 import express from 'express'
 const app = express();
 
+import { InstallProvider } from '@slack/oauth';
+
+// initialize the installProvider
+const installer = new InstallProvider({
+  clientId: process.env.SLACK_CLIENT_ID!,
+  clientSecret: process.env.SLACK_CLIENT_SECRET!,
+  stateSecret: process.env.SLACK_STATE_SECRET!,
+  renderHtmlForInstallPath: (url) => `<html><body><a href="${url}">Install my app!</a></body></html>`,
+  installUrlOptions: {
+    scopes: [
+      "chat:write",
+      "channels:manage",
+      "groups:write",
+      "im:write",
+      "mpim:write",
+    ],
+    userScopes: [
+      "channels:write",
+      "im:write",
+      "mpim:write",
+      "groups:write",
+      "users.profile:write",
+      "users.profile:read",
+    ]
+  }
+
+});
+
+app.get('/slack/install', async (req, res) => {
+  await installer.handleInstallPath(req, res);
+});
+
+
+app.get('/slack/oauth_redirect', (req, res) => {
+  installer.handleCallback(req, res);
+});
+
+
 //redicrect
 app.get('slack/oauth_redirect', function (req, res, next) {
   res.send('<h1>Redirect</h1>');
 });
 
-
-//install
-app.get('/slack/install', async (req, res) => {
-  res.send('<h1>Install</h1>\n<a href="https://google.com/">サンプルサイトへ飛びます。</a>' + process.env.TEST);
-});
 
 //API
 app.get('/api/:memberId', function(req,res,next){
@@ -20,6 +53,6 @@ app.get('/api/:memberId', function(req,res,next){
 });
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
   console.log("Start Express on port 3000.");
 });
