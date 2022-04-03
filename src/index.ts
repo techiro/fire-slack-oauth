dc4bd82eb5f0a90b2109030c71acf4a80fdde0a9
techiro <techiromen0@gmail.com>
2022-04-03 16:54:59 +0900

    :sparkles: サンプルexpressを実装

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
-}
+import express from 'express'
+const app = express();
 
-console.log(hello("TypeScript"));
+//redicrect
+app.get('slack/oauth_redirect', function (req, res, next) {
+  res.send('<h1>Redirect</h1>');
+});
+
+
+//install
+app.get('/slack/install', async (req, res) => {
+  res.send('<h1>Install</h1>\n<a href="https://google.com/">サンプルサイトへ飛びます。</a>');
+});
+
+//API
+app.get('/api/:memberId', function(req,res,next){
+  var memberId = req.params.memberId;
+  var data = {"status":"OK","memberId":memberId}
+  res.json(data);
+});
+
+
+app.listen(3000, function(){
+  console.log("Start Express on port 3000.");
+});
